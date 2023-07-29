e6ec845bed100ea7dfda6d2f90de40f3022d1dc2
yunhaofu <yunhaofu_nuist@163.com>
2023-07-29 21:38:48 +0800

    bound check only for a given val, not for variable

--- ubound.f90
+++ ubound.f90
@@ -5,10 +5,12 @@ integer :: array(-4:1), test
 array(:) = 1
 
 test = 2
-array(test)  = 2 ! ifort no error
+array(test)  = 2 ! ifort no error: 
                  ! gfortran no error
 
 array(2)  = 2  ! ifort no error
+               ! to check: -check bounds -traceback -g
+
                ! gfortran Warning: Array reference at (1) is out of bounds (2 > 1) in dimension 1
 
 print *, array
