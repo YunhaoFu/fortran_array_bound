ccc5c52aa15251040c270d77d57de725714cf70a
yunhaofu <yunhaofu_nuist@163.com>
2023-07-30 16:42:19 +0800

    update ifort&gfortran bound check

--- ubound.f90
+++ ubound.f90
@@ -5,14 +5,17 @@ integer :: array(-4:1), test
 array(:) = 1
 
 test = 2
-array(test)  = 2 ! ifort no error: 
-                 ! gfortran no error
+array(test) = 2 ! ifort no error in compile
+                ! gfortran no error in compile
 
-array(2)  = 2  ! ifort no error
-               ! to check: -check bounds -traceback -g
+array(2)  = 2   ! ifort no error
+                ! to check: -check bounds -traceback -g
+                ! terminated in run
 
-               ! gfortran Warning: Array reference at (1) is out of bounds (2 > 1) in dimension 1
+                ! gfortran Warning: Array reference at (1) is out of bounds (2 > 1) in dimension 1
+                ! to check: -fbounds-check
+                ! terminated in run
 
-print *, array
+!print *, array(-4:2)
 
 end
